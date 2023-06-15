e2c5026a62ca93245cc254f0da2236b8d5f9636f
spreekaiser <thoralf.barth@gmail.com>
2023-06-15 16:00:27 +0200

    startpoint of Schifffahrten

---
+++ pages/tourguide.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Tourguide() {
+  return (
+    <>
+      <Head>
+        <title>Audio Tourguide</title>
+        <meta
+          name="description"
+          lang="en"
+          content="Audio tourguide for boat trips in Berlin"
+        />
+        <meta
+          name="description"
+          lang="de"
+          content="Audio tourguide für Schifffahrten in Berlin"
+        />
+      </Head>
+    </>
+  );
+}
